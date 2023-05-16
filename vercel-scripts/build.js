765f5f3f1622006bdae444149eecb0c236ec7f15
Jin <devjin88@gmail.com>
2023-05-17 07:07:26 +0800

    Use own vercel scripts

---
+++ vercel-scripts/build.js
@@ -0,0 +1,5 @@
+const {version, downloadFilesInWorkPath, build, shouldServe, installRequirement, installRequirementsFile} = require('@vercel/python')
+
+export {
+  version, downloadFilesInWorkPath, build, shouldServe, installRequirement, installRequirementsFile
+}
